c8105f5573f119974a8e1281ddb53f20295f70f6
Geno <romanelli.geno@gmail.com>
2017-02-04 12:00:07 -0500

    Chapter 9 - forms

--- leaderboard.js
+++ leaderboard.js
@@ -24,6 +24,7 @@ if(Meteor.isClient){
       return PlayersList.findOne({ _id: selectedPlayer});
     }
   });
+
   Template.leaderboard.events({
     'click .player': function(){
       var playerId = this._id;
@@ -36,6 +37,22 @@ if(Meteor.isClient){
     'click .decrement': function(){
       var selectedPlayer = Session.get('selectedPlayer');
       PlayersList.update({_id: selectedPlayer} , { $inc: {score: -5}});
+    },
+    'click .remove': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.remove({ _id: selectedPlayer});
+    }
+  });
+
+  Template.addPlayerForm.events({
+    'submit form': function(event){
+      event.preventDefault();
+      var playerNameVar = event.target.playerName.value;
+      PlayersList.insert({
+        name: playerNameVar,
+        score: 0
+      });
+      event.target.playerName.value = "";
     }
   });
 
