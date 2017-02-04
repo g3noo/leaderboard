5397aec32c0d6e6271d021a812f492d7e1c4d527
Geno <romanelli.geno@gmail.com>
2017-02-04 11:41:41 -0500

    Chapter 8

--- leaderboard.js
+++ leaderboard.js
@@ -4,7 +4,7 @@ PlayersList = new Mongo.Collection('players');
 if(Meteor.isClient){
   Template.leaderboard.helpers({
     'player': function(){
-      return PlayersList.find();
+      return PlayersList.find({}, {sort: {score: -1, name: 1}});
     },
     'otherHelperFunction': function(){
       return "Some other function";
@@ -16,14 +16,26 @@ if(Meteor.isClient){
       var playerId = this._id;
       var selectedPlayer = Session.get('selectedPlayer');
       if(playerId == selectedPlayer){
-        return "selected"  
+        return "selected"
       }
+    },
+    'selectedPlayer': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      return PlayersList.findOne({ _id: selectedPlayer});
     }
   });
   Template.leaderboard.events({
     'click .player': function(){
       var playerId = this._id;
       Session.set('selectedPlayer', playerId);
+    },
+    'click .increment': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.update({_id: selectedPlayer} , { $inc: {score: 5}});
+    },
+    'click .decrement': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.update({_id: selectedPlayer} , { $inc: {score: -5}});
     }
   });
 
